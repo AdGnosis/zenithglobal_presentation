194dd9fec03e5009e6fce8b957e8886d4b3e7186
Ad Gnosis <adgnosiscontact@gmail.com>
2025-12-14 21:02:45 +0530

    Delete components directory

--- components/SlideProgress.tsx
+++
@@ -1,8 +0,0 @@
-
-export default function SlideProgress({ current, total }: { current: number; total: number }) {
-  return (
-    <div style={{ position: "fixed", bottom: 10, left: 10, fontSize: 12 }}>
-      Slide {current + 1} / {total}
-    </div>
-  );
-}
